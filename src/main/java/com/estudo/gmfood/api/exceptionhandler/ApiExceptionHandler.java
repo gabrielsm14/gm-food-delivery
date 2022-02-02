1040c058f041a052e492f147b1bba75606192e6b
Gabriel <gabriel.mendonca14@outlook.com>
2022-02-01 22:49:56 -0300

    Padronizando formato de problemas no corpo de respostas

--- src/main/java/com/estudo/gmfood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/estudo/gmfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -42,14 +42,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler { // Res
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<?> handleNegocioExecption(NegocioException ex, WebRequest request) {
 
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		Problem problem = createProblemBuilder(status, ProblemType.ERRO_NEGOCIO, ex.getMessage()).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
 	}
 
 	@ExceptionHandler(EntidadeEmUsoException.class)
 	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
 
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+		HttpStatus status = HttpStatus.CONFLICT;
+		
+		Problem problem = createProblemBuilder(status, ProblemType.ENTIDADE_EM_USO, ex.getMessage()).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
 	@Override
