350f361c19475a587ea2da3bbba3c2c33ac540be
Gabriel <gabriel.mendonca14@outlook.com>
2022-02-26 22:03:19 -0300

    Criando constraints de validação customizadas em nivel de classe

--- src/main/java/com/estudo/gmfood/domain/model/Restaurante.java
+++ src/main/java/com/estudo/gmfood/domain/model/Restaurante.java
@@ -26,6 +26,7 @@ import javax.validation.groups.Default;
 import com.estudo.gmfood.core.validation.Groups;
 import com.estudo.gmfood.core.validation.Multiplo;
 import com.estudo.gmfood.core.validation.TaxaFrete;
+import com.estudo.gmfood.core.validation.ValorZeroIncluiDescricao;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -34,56 +35,57 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@ValorZeroIncluiDescricao(valorField = "taxaFrete", descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotBlank
-	@Column(nullable = false)
-	private String nome;
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
 
-	@NotNull
-	@PositiveOrZero
-	@Column(name = "taxa_frete", nullable = false)
-	private BigDecimal taxaFrete;
-	
-//	@JsonIgnoreProperties("hibernateLazyInitializer")
-	//@JsonIgnore
-	@Valid
-	@NotNull
-	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class) // na hora de validar cozinha converte do group defaut para Groups.cadastroRestaurante.class
-	@ManyToOne //(fetch = FetchType.LAZY) // ToOne por padrao é Eager
-	@JoinColumn(name = "cozinha_id", nullable = false)
-	private Cozinha cozinha;
-	
-	@JsonIgnore
-	@Embedded // é uma parte da class restaurante
-	private Endereco endereco;
-	
-	@JsonIgnore
-	@CreationTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataCadastro;
-	
-	@JsonIgnore
-	@UpdateTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataAtualizacao;
-	
-	@JsonIgnore
-	@ManyToMany // ToMany por padrao é Lazy
-	@JoinTable(name = "restaurante_forma_pagamento",
-		joinColumns = @JoinColumn(name = "restaurante_id"),
-		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    @NotNull
+    @PositiveOrZero
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
 
-	@JsonIgnore
-	@OneToMany(mappedBy = "restaurante") 
-	private List<Produto> produtos = new ArrayList<>();
+    //	@JsonIgnoreProperties("hibernateLazyInitializer")
+    //@JsonIgnore
+    @Valid
+    @NotNull
+    @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)// na hora de validar cozinha converte do group defaut para Groups.cadastroRestaurante.class
+    @ManyToOne //(fetch = FetchType.LAZY) // ToOne por padrao é Eager
+    @JoinColumn(name = "cozinha_id", nullable = false)
+    private Cozinha cozinha;
+
+    @JsonIgnore
+    @Embedded // é uma parte da class restaurante
+    private Endereco endereco;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
+
+    @JsonIgnore
+    @ManyToMany // ToMany por padrao é Lazy
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurante")
+    private List<Produto> produtos = new ArrayList<>();
 }
