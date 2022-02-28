b03c64112f4f71768ad3ea1294b5281cd6e934da
Gabriel <gabriel.mendonca14@outlook.com>
2022-02-27 23:04:54 -0300

    Removendo imports

--- src/main/java/com/estudo/gmfood/domain/model/Restaurante.java
+++ src/main/java/com/estudo/gmfood/domain/model/Restaurante.java
@@ -1,39 +1,23 @@
 package com.estudo.gmfood.domain.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import com.estudo.gmfood.core.validation.Groups;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
-
-import com.estudo.gmfood.core.validation.Groups;
-import com.estudo.gmfood.core.validation.Multiplo;
-import com.estudo.gmfood.core.validation.TaxaFrete;
-import com.estudo.gmfood.core.validation.ValorZeroIncluiDescricao;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
