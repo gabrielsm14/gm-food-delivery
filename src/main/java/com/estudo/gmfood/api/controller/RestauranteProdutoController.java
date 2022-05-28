f6e11c8eb911dddfcb38f380722fbfa5e4df62c5
Gabriel Mendonca Pessoal <gabrielmendoncapessoal@MacBook-Pro-de-picpay.local>
2022-05-27 21:12:07 -0300

    Implementando endpoints de produtos

---
+++ src/main/java/com/estudo/gmfood/api/controller/RestauranteProdutoController.java
@@ -0,0 +1,77 @@
+package com.estudo.gmfood.api.controller;
+
+import com.estudo.gmfood.api.assembier.ProdutoInputDisassembler;
+import com.estudo.gmfood.api.assembier.ProdutoRequestAssembler;
+import com.estudo.gmfood.api.model.ProdutoRequest;
+import com.estudo.gmfood.api.model.input.ProdutoInput;
+import com.estudo.gmfood.domain.model.Produto;
+import com.estudo.gmfood.domain.model.Restaurante;
+import com.estudo.gmfood.domain.repository.ProdutoRepository;
+import com.estudo.gmfood.domain.service.CadastroRestauranteService;
+import com.estudo.gmfood.domain.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurante/{restauranteId}/produtos")
+public class RestauranteProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private CadastroRestauranteService restauranteService;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private ProdutoRequestAssembler produtoRequestAssembler;
+
+    @Autowired
+    private ProdutoInputDisassembler produtoInputDisassembler;
+
+    @GetMapping
+    public List<ProdutoRequest> listar(@PathVariable Long restauranteId) {
+        Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
+
+        List<Produto> todosProdutos = produtoRepository.findByRestaurante(restaurante);
+
+        return produtoRequestAssembler.toCollectionModel(todosProdutos);
+    }
+
+    @GetMapping("/{produtoId}")
+    public ProdutoRequest buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+        Produto produto = produtoService.buscarOuFalhar(restauranteId, produtoId);
+
+        return produtoRequestAssembler.toModel(produto);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProdutoRequest adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
+        Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
+
+        Produto produto = produtoInputDisassembler.toDomainObject(produtoInput);
+        produto.setRestaurante(restaurante);
+
+        produto = produtoService.salvar(produto);
+
+        return produtoRequestAssembler.toModel(produto);
+    }
+
+    @PutMapping("{produtoId}")
+    public ProdutoRequest atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId, @RequestBody @Valid ProdutoInput produtoInput) {
+        Produto produtoAtual = produtoService.buscarOuFalhar(restauranteId, produtoId);
+
+        produtoInputDisassembler.copyToDomainObject(produtoInput, produtoAtual);
+
+        produtoAtual = produtoService.salvar(produtoAtual);
+
+        return produtoRequestAssembler.toModel(produtoAtual);
+    }
+}
---
+++ src/main/java/com/estudo/gmfood/domain/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package com.estudo.gmfood.domain.service;
+
+import com.estudo.gmfood.domain.exception.ProdutoNaoEncontradaException;
+import com.estudo.gmfood.domain.model.Produto;
+import com.estudo.gmfood.domain.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProdutoService {
+
+    private static final String MSG_USUARIO_EM_USO = "Produto de código %d não pode ser removida, pois está em uso";
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId).orElseThrow(() -> new ProdutoNaoEncontradaException(restauranteId, produtoId));
+    }
+}
